28cf8bef893ef43783b344efe65c49fe8a430e73
Ihor Robeiko <rid@ukr.net>
2022-12-26 23:32:46 +0200

    Change app info: short text

--- src/master/Main.java
+++ src/master/Main.java
@@ -3,23 +3,16 @@ package master;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("My app for Users!");
-        System.out.println("Version 1.0");
+        System.out.println("Super app!");
         int a = 8;
         int b = 2;
         int c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
-        System.out.println(d);
     }
 
     private static int add(int a, int b) {
 
         return a + b;
-    }
-    private static int multiply(int a, int b) {
-
-        return a * b;
     }
 }
 
